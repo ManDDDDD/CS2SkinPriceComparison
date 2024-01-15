e0b11bb5d0c5ba2c0a81d03f3575499d96571082
Cigdan <72510831+Cigdan@users.noreply.github.com>
2024-01-15 22:28:03 +0100

    Fixed bugs and added base for testing

---
+++ CS2SkinPriceComparison/TestProject1/SeleniumTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS2SkinPriceComparison;
+
+namespace CS2SkinPriceComparison.Tests
+{
+    [TestClass]
+    public class SeleniumTests
+    {
+        [TestMethod]
+        public void TestGetPricesFromSkinPort()
+        {
+            // Arrange
+            var selenium = new Selenium();
+            var skin = new Skin
+            {
+                Name = "TestSkin",
+                Category = "Gun Skin",
+                SubCategory = "Rifle",
+                Item = "AK-47"
+            };
+
+            // Act
+            string price = selenium.GetPricesFromSkinPort(skin);
+
+            // Assert
+            Assert.IsNotNull(price, "Price should not be null");
+            // Add more assertions based on the expected behavior of GetPricesFromSkinPort
+        }
+
+        [TestMethod]
+        public void TestDefineSkinPortUrl()
+        {
+            // Arrange
+            var selenium = new Selenium();
+            var skin = new Skin
+            {
+                Name = "TestSkin",
+                Category = "Gun Skin",
+                SubCategory = "Rifle",
+                Item = "AK-47"
+            };
+
+            // Act
+            string url = selenium.DefineSkinPortUrl(skin);
+
+            // Assert
+            Assert.IsNotNull(url, "URL should not be null");
+            // Add more assertions based on the expected behavior of DefineSkinPortUrl
+        }
+
+        [TestMethod]
+        public void TestGetPricesFromSkinBaron()
+        {
+            // Arrange
+            var selenium = new Selenium();
+            var skin = new Skin
+            {
+                Name = "TestSkin",
+                Category = "Gun Skin",
+                SubCategory = "Rifle",
+                Item = "AK-47"
+            };
+
+            // Act
+            string price = selenium.GetPricesFromSkinBaron(skin);
+
+            // Assert
+            Assert.IsNotNull(price, "Price should not be null");
+            // Add more assertions based on the expected behavior of GetPricesFromSkinBaron
+        }
+
+        [TestMethod]
+        public void TestDefineSkinBaronUrl()
+        {
+            // Arrange
+            var selenium = new Selenium();
+            var skin = new Skin
+            {
+                Name = "TestSkin",
+                Category = "Gun Skin",
+                SubCategory = "Rifle",
+                Item = "AK-47"
+            };
+
+            // Act
+            string url = selenium.DefineSkinBaronUrl(skin);
+
+            // Assert
+            Assert.IsNotNull(url, "URL should not be null");
+            // Add more assertions based on the expected behavior of DefineSkinBaronUrl
+        }
+
+        [TestMethod]
+        public void SpaceToHyphen_ShouldReplaceSpacesWithHyphens()
+        {
+            // Arrange
+            var selenium = new Selenium();
+            string input = "Test Skin";
+
+            // Act
+            string result = selenium.SpaceToHyphen(input);
+
+            // Assert
+            Assert.AreEqual("Test-Skin", result, "Result should match");
+            // Add more assertions based on the expected behavior of SpaceToHyphen
+        }
+
+        [TestMethod]
+        public void SpaceToPlus_ShouldReplaceSpacesWithPluses(string input, string expectedResult)
+        {
+            // Arrange
+            var selenium = new Selenium();
+
+            // Act
+            string result = selenium.SpaceToPlus(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result, "Result should match");
+            // Add more assertions based on the expected behavior of SpaceToPlus
+        }
+
+        [TestMethod]
+        [DataRow("test", "Test")]
+        [DataRow("Test", "Test")]
+        [DataRow(" asiimov", "Asiimov")]
+        
+        public void FirstCharToUpper_ShouldCapitalizeFirstLetter(string input, string expectedResult)
+        {
+            // Arrange
+            var selenium = new Selenium();
+
+            // Act
+            string result = selenium.FirstCharToUpper(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result, "Result should match");
+            // Add more assertions based on the expected behavior of FirstCharToUpper
+        }
+    }
+}
---
+++ CS2SkinPriceComparison/TestProject1/UserInputTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS2SkinPriceComparison;
+using Newtonsoft.Json.Linq;
+
+namespace CS2SkinPriceComparison.Tests
+{
+    [TestClass]
+    public class UserInputTests
+    {
+
+        [TestMethod]
+        public void TestGetCHFPriceFromEUR()
+        {
+            // Arrange
+            var userInput = new UserInput();
+
+            // Act
+            var result = userInput.GetCHFPriceFromEUR(100, "EUR").Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Result should not be null");
+            Assert.IsTrue(result > 0, "Result should be greater than 0");
+            // Add more assertions based on the expected behavior of GetCHFPriceFromEUR
+        }
+    }
+}
